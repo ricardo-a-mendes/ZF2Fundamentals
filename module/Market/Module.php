e504bf837e5643c5c0ed8d50dfc7f3c1fec48eec
Ricardo Mendes <eng.rmendes@gmail.com>
2015-02-23 17:34:08 -0300

    Ex1A do modulo 5 - M5x1 - A

---
+++ module/Market/Module.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Market;
+
+class Module
+{
+    public function getConfig()
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
+
+    public function getAutoloaderConfig()
+    {
+        return array(
+            //Feito para aumentar a performance
+            'Zend\Loader\ClassMapAutoloader' => array(
+                __DIR__ . '/autoload_classmap.php'
+            ),
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                ),
+            ),
+        );
+    }
+
+}
